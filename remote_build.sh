a192e2b679706d413e59148001e72603f225e78f
estebanqe <157732736+estebanqe@users.noreply.github.com>
2024-10-12 19:40:18 -0500

    remote_buils.sh

---
+++ remote_build.sh
@@ -0,0 +1,11 @@
+cd /vscode/prueba_func
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+rm -rf public
+reflex init
+API_URL=pruebafunci-production.up.railway.app reflex export --frontend-only
+unzip frontend.zip -d public
+rm -f frontend.zip
+deactivate
