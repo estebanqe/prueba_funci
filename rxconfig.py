08d890433be2bd4a0ce9afee17a15c90391676c5
estebanqe <157732736+estebanqe@users.noreply.github.com>
2024-10-14 19:52:34 -0500

    intento 1 de despliegue back

--- rxconfig.py
+++ rxconfig.py
@@ -4,6 +4,6 @@ config = rx.Config(
     app_name="prueba_func",
     cors_allowed_origins=[
         "http://localhost:3000",
-        "prueba-funci.vercel.app"
+        "https://prueba-funci.vercel.app"
     ]
 )
