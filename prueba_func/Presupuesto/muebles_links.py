6995e773c75fcb8e5d971bcaed4ad8023a8a0d64
estebanqe <157732736+estebanqe@users.noreply.github.com>
2024-10-30 18:27:31 -0500

    Update muebles_links.py

--- prueba_func/Presupuesto/muebles_links.py
+++ prueba_func/Presupuesto/muebles_links.py
@@ -1,5 +1,7 @@
 import reflex as rx
+import prueba_func.constants as const
 from prueba_func.estilo.estilo import Color, TextColor, Spacing, Size
+from prueba_func.components.link_material import link_material
 from prueba_func.state.PageState import PageState
 
 def muebles_links() -> rx.Component:
@@ -14,7 +16,18 @@ def muebles_links() -> rx.Component:
             PageState.mueble_fila_info,
             lambda mueble: rx.tabs.content(
                 rx.text(f"Descripción para {mueble}",color=TextColor.BODY.value),
-                value=mueble
+                rx.vstack(
+                    rx.button(f"el botton de {mueble} #1",color=TextColor.BODY.value),
+                     link_material(
+                        "Mueble",
+                        "dimension 2.12 x 2.44 m",
+                        "/modelos/escritorio-melamina.jpg"
+                    ),
+                ),
+                rx.button(f"el botton de {mueble} #2",color=TextColor.BODY.value),
+                value=mueble,
+                align_items="start",
+                width="100%",
             )
         ),
         default_value=rx.cond(
