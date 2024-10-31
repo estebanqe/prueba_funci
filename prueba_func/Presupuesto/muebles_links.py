5dc0e8260ce3aeea579e06e9d2b191bbb333a111
estebanqe <157732736+estebanqe@users.noreply.github.com>
2024-10-30 19:28:45 -0500

    Update muebles_links.py

--- prueba_func/Presupuesto/muebles_links.py
+++ prueba_func/Presupuesto/muebles_links.py
@@ -9,26 +9,41 @@ def muebles_links() -> rx.Component:
         rx.tabs.list(
             rx.foreach(
                 PageState.mueble_fila_info,
-                lambda mueble: rx.tabs.trigger(rx.text(mueble), value=mueble,color=TextColor.BODY.value)
+                lambda mueble: 
+                        rx.tabs.trigger(
+                            rx.text(mueble),
+                            value=mueble,
+                            color=TextColor.BODY.value
+                        )
             )
         ),
         rx.foreach(
             PageState.mueble_fila_info,
-            lambda mueble: rx.tabs.content(
-                rx.text(f"Descripción para {mueble}",color=TextColor.BODY.value),
-                rx.vstack(
-                    rx.button(f"el botton de {mueble} #1",color=TextColor.BODY.value),
-                     link_material(
-                        "Mueble",
-                        "dimension 2.12 x 2.44 m",
-                        "/modelos/escritorio-melamina.jpg"
+            lambda mueble:
+                rx.tabs.content(
+                    rx.text(
+                        f"Descripción para {mueble}",
+                        color=TextColor.BODY.value
+                        ),
+                    rx.vstack(
+                        rx.button(
+                            f"el botton de {mueble} #1",
+                            color=TextColor.BODY.value
+                        ),
+                        link_material(
+                            "Mueble",
+                            "dimension 2.12 x 2.44 m",
+                            "/modelos/escritorio-melamina.jpg"
+                        ),
                     ),
-                ),
-                rx.button(f"el botton de {mueble} #2",color=TextColor.BODY.value),
-                value=mueble,
-                align_items="start",
-                width="100%",
-            )
+                    rx.button(
+                        f"el botton de {mueble} #2",
+                        color=TextColor.BODY.value
+                    ),
+                    value=mueble,
+                    align_items="start",
+                    width="100%",
+                )
         ),
         default_value=rx.cond(
             PageState.mueble_fila_info,
