0eec2a580a9b8018199eeae8918b14f7b95ab6f0
esteban <estebanquinae@gmail.com>
2024-11-27 17:54:11 -0500

    SuperCotizar 2

--- prueba_func/api/Supacotizar.py
+++ prueba_func/api/Supacotizar.py
@@ -2,27 +2,18 @@ import os
 import dotenv
 from supabase import create_client, Client
 
-dotenv.load_dotenv()
+
 
 class BaseAPI:
     
+
     dotenv.load_dotenv()
-    
-    def __init__(self):
-        self.supabase_url = os.getenv("SUPABASE_URL")
-        self.supabase_key = os.getenv("SUPABASE_KEY")
 
-        if not self.supabase_url or not self.supabase_key:
-            raise ValueError("Faltan las credenciales de Supabase")
+    SUPABASE_URL = os.environ.get("SUPABASE_URL")
+    SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
-        self.client: Client = create_client(self.supabase_url, self.supabase_key)
-
-
-    def execute_query(self, query: str):
-        """Ejecuta una consulta SQL en Supabase."""
-        try:
-            response = self.client.rpc("execute_query", {"sql": query})
-            return response.data
-        except Exception as e:
-            print(f"Error ejecutando consulta: {e}")
-            return []
+    def __init__(self) -> None:
+        if self.SUPABASE_URL != None and self.SUPABASE_KEY != None:
+            self.client: Client = create_client(
+                self.SUPABASE_URL, self.SUPABASE_KEY
+            )
