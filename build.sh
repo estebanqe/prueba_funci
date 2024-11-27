5a3e0e00ead8d8734fede5a8345d06511bab0907
esteban <estebanquinae@gmail.com>
2024-11-27 15:34:36 -0500

    Añadir script build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+cd //wsl.localhost/Ubuntu-22.04/home/esteban/vscode/prueba_func
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+rm -rf public
+reflex init
+API_URL=https://pruebafunci-production.up.railway.app/ 
+reflex export --frontend-only
+unzip frontend.zip -d public
+rm -f frontend.zip
+deactivate
